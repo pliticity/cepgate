376540fadf7586eef301234d821607336e2781fa
pmajchrz <dachoin@gmail.com>
2016-06-01 11:55:01 +0200

    cx-55 Document Information Carrier - Document Responsible

--- src/main/java/pl/iticity/dbfds/controller/PrincipalController.java
+++ src/main/java/pl/iticity/dbfds/controller/PrincipalController.java
@@ -1,4 +1,4 @@
-`package pl.iticity.dbfds.controller;
+package pl.iticity.dbfds.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
