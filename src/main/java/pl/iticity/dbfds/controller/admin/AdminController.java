76480c8b16fd94e994af10b6e5bfe4312445a8b8
pmajchrz <dachoin@gmail.com>
2016-06-22 10:44:13 +0200

    CG-59 Global Admin and Admin add account/domain fields

--- src/main/java/pl/iticity/dbfds/controller/admin/AdminController.java
+++ src/main/java/pl/iticity/dbfds/controller/admin/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.omg.CORBA.UnknownUserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.iticity.dbfds.model.Domain;
+import pl.iticity.dbfds.security.AuthorizationProvider;
 import pl.iticity.dbfds.security.Role;
 import pl.iticity.dbfds.service.DomainService;
 import pl.iticity.dbfds.service.FileService;
@@ -41,9 +42,12 @@ public class AdminController{
     public
     @ResponseBody
     List<Domain> getDomains() {
+        AuthorizationProvider.hasRole(Role.GLOBAL_ADMIN,null);
         List<Domain> domains = domainService.findAll();
         for (Domain domain : domains) {
             domain.setNoOfUsers(principalService.findByDomain(domain).size());
+            domain.setNoOfFiles(fileService.countByDomain(domain));
+            domain.setMemory(fileService.countMemoryByDomain(domain));
         }
         return domains;
     }
@@ -52,11 +56,24 @@ public class AdminController{
     public
     @ResponseBody
     Domain getDomain(@PathVariable("id") String id) {
+        AuthorizationProvider.hasRole(Role.GLOBAL_ADMIN,null);
         Domain domain = domainService.findById(id);
         domain.setPrincipals(principalService.findByDomain(domain));
         domain.setNoOfFiles(fileService.countByDomain(domain));
         domain.setMemory(fileService.countMemoryByDomain(domain));
+        domain.setNoOfUsers(domain.getPrincipals().size());
         return domain;
+    }
+
+    @RequestMapping(value = "domain/{id}",method = RequestMethod.PUT, params = {"active"})
+    public
+    @ResponseBody
+    List<Domain> putToggleDomainActive(@PathVariable("id") String id, @RequestParam("active") boolean active) {
+        AuthorizationProvider.hasRole(Role.GLOBAL_ADMIN,null);
+        Domain domain = domainService.findById(id);
+        domain.setActive(active);
+        domainService.save(domain);
+        return getDomains();
     }
 
 
