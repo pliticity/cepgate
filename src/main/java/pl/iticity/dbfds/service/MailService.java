e65836d98fd62864b21441169d3b7a1b93cd5f53
pmajchrz <dachoin@gmail.com>
2016-07-13 14:48:50 +0200

    CG-55 Mark documents and send them on e-mail

--- src/main/java/pl/iticity/dbfds/service/MailService.java
+++ src/main/java/pl/iticity/dbfds/service/MailService.java
@@ -1,6 +1,11 @@
 package pl.iticity.dbfds.service;
 
 import org.apache.log4j.Logger;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailParseException;
 import org.springframework.mail.MailSender;
@@ -11,6 +16,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import pl.iticity.dbfds.model.DocumentInfo;
 import pl.iticity.dbfds.model.FileInfo;
+import pl.iticity.dbfds.repository.DocumentInfoRepository;
 import pl.iticity.dbfds.security.AuthorizationProvider;
 import pl.iticity.dbfds.util.DefaultConfig;
 import pl.iticity.dbfds.util.PrincipalUtils;
@@ -18,6 +24,7 @@ import pl.iticity.dbfds.util.PrincipalUtils;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.io.StringWriter;
 import java.text.MessageFormat;
 
 @Service
@@ -35,6 +42,9 @@ public class MailService {
     private DefaultConfig defaultConfig;
 
     @Autowired
+    private DocumentInfoRepository documentInfoRepository;
+
+    @Autowired
     private FileService fileService;
 
     public void sendDocument(String docId, String[] files, boolean zip) {
@@ -46,11 +56,16 @@ public class MailService {
 
             helper.setFrom(defaultConfig.getSmtpFrom());
             helper.setTo(PrincipalUtils.getCurrentPrincipal().getEmail());
-            helper.setSubject("files");
+            helper.setSubject("This is an e-mail generated from Cepgate.");
+
+            StringBuilder text = new StringBuilder("This is an e-mail generated from Cepgate.");
+            for (String f : files) {
+                DocumentInfo doc = documentInfoRepository.findByFiles_Id(f);
+                text.append(fillTemplate(doc));
+            }
+            helper.setText(text.toString(),true);
 
             if (!zip) {
-                helper.setText("files");
-
                 for (String f : files) {
                     FileInfo fileInfo = fileService.findById(f);
                     File file = fileService.getFileForFileInfo(fileInfo);
@@ -61,9 +76,29 @@ public class MailService {
                 String msg = MessageFormat.format("<a href=\"http://webapp-cepgate.rhcloud.com/file/{0}?temp=true\">link to files</a> this is a one time link",fileInfo.getSymbol());
                 helper.setText(msg,true);
             }
+
+
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
         mailSender.send(mail);
+    }
+
+    private String fillTemplate(DocumentInfo documentInfo){
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
+        ///home/pmajchrz/other/dbfds/dbfds/src/main/
+        Template t = ve.getTemplate( "/templates/mail/files.vm" );
+        VelocityContext context = new VelocityContext();
+        context.put("classificationId", documentInfo.getClassification().getClassificationId());
+        context.put("classificationName", documentInfo.getClassification().getName());
+        context.put("documentTitle", documentInfo.getDocumentName());
+        context.put("documentNumber", documentInfo.getDocumentNumber());
+        context.put("revision", documentInfo.getRevision().getEffective());
+        StringWriter writer = new StringWriter();
+        t.merge( context, writer );
+        return writer.toString();
     }
 }
