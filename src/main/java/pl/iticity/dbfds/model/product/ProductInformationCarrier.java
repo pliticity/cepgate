1246382f3fa72590098d6f57a467b37b06c6fdaf
pmajchrz <dachoin@gmail.com>
2016-07-19 14:51:20 +0200

    CG-83 PIC - Product Module

---
+++ src/main/java/pl/iticity/dbfds/model/product/ProductInformationCarrier.java
@@ -0,0 +1,38 @@
+package pl.iticity.dbfds.model.product;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import pl.iticity.dbfds.model.Classification;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@org.springframework.data.mongodb.core.mapping.Document(collection = "products")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductInformationCarrier {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @ManyToOne
+    @NotNull
+    @DBRef
+    private Classification classification;
+
+    private String name;
+
+    private String productId;
+
+    private String weight;
+
+    private String codeA;
+
+    private String codeB;
+
+    private String color;
+
+
+}
