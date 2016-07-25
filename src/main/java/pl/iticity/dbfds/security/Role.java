32e3eb9543256080f380fd5ea720fcffe3b44f3c
pmajchrz <dachoin@gmail.com>
2016-07-25 10:40:04 +0200

    CG-93 Move Security/User grous list from Setup to Users tab

--- src/main/java/pl/iticity/dbfds/security/Role.java
+++ src/main/java/pl/iticity/dbfds/security/Role.java
@@ -3,6 +3,6 @@ package pl.iticity.dbfds.security;
 
 public enum Role {
 
-    ADMIN, CLIENT, USER, DOCUMENT_CONTROL, SALES, MANAGEMENT, GLOBAL_ADMIN
+    ADMIN, CLIENT, USER, SALES, MANAGEMENT, GLOBAL_ADMIN
 
 }
