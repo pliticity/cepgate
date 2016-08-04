8918b86db91aaf642420eac8a3615f1398e9041b
pmajchrz <dachoin@gmail.com>
2016-08-04 11:31:00 +0200

    CG-56 Document sent with Transmittals

--- src/main/java/pl/iticity/dbfds/service/common/impl/TransmittalServiceImpl.java
+++ src/main/java/pl/iticity/dbfds/service/common/impl/TransmittalServiceImpl.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDSimpleFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
@@ -37,11 +38,13 @@ public class TransmittalServiceImpl implements TransmittalService {
 
     public File createTransmittal(Principal sender, Principal recipient, java.util.List<DocumentInfo> documentInfo, Mail mail) throws IOException {
         PDDocument doc = new PDDocument();
-        PDPage page = new PDPage();
+        PDPage page = new PDPage(PDRectangle.A4);
+        page.setRotation(90);
 
         doc.addPage(page);
 
         PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+        contentStream.concatenate2CTM(0, 1, -1, 0, PDRectangle.A4.getHeight(), 0);
 
         drawTable(contentStream,sender,recipient,documentInfo,mail);
 
@@ -53,18 +56,18 @@ public class TransmittalServiceImpl implements TransmittalService {
     }
 
     private void drawTable(PDPageContentStream contentStream, Principal sender, Principal recipient, java.util.List<DocumentInfo> documentInfo, Mail mail) throws IOException {
-        int lineY = 700;
+        int lineY = 800;
         PDFont font = PDType1Font.HELVETICA;
         contentStream.setFont(font, 12);
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(450, lineY);
+        contentStream.newLineAtOffset(700, lineY);
         contentStream.showText("Transmittal Letter");
         contentStream.endText();
         lineY-=13;
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(450, lineY);
+        contentStream.newLineAtOffset(700, lineY);
         contentStream.showText(MessageFormat.format("Date : {0}", new LocalDate()));
         contentStream.endText();
         lineY-=13;
@@ -76,7 +79,7 @@ public class TransmittalServiceImpl implements TransmittalService {
         contentStream.endText();
 
         contentStream.setStrokingColor(Color.BLACK);
-        contentStream.addRect(100,lineY-2,400f,1f);
+        contentStream.addRect(50,lineY-2,700f,1f);
         contentStream.stroke();
 
         lineY-=13;
@@ -89,7 +92,7 @@ public class TransmittalServiceImpl implements TransmittalService {
         contentStream.endText();
 
         contentStream.setStrokingColor(Color.BLACK);
-        contentStream.addRect(100,lineY-2,400f,1f);
+        contentStream.addRect(50,lineY-2,700f,1f);
         contentStream.stroke();
 
         lineY-=13;
@@ -97,15 +100,15 @@ public class TransmittalServiceImpl implements TransmittalService {
         lineY = addCompanyInfo(contentStream,mail.getCompany(),mail.getAddress(),mail.getPhone(),lineY);
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(390, tempLine);
+        contentStream.newLineAtOffset(640, tempLine);
         contentStream.showText("Received Signature");
         contentStream.endText();
 
         tempLine-=13;
-        contentStream.addRect(390,tempLine,120f,-60f);
+        contentStream.addRect(640,tempLine,120f,-60f);
         contentStream.stroke();
 
-        lineY-=93;
+        lineY-=163;
 
         lineY = addDocRow(contentStream,"Document number","Document Type","Document Title","Revision",lineY);
 
@@ -120,22 +123,22 @@ public class TransmittalServiceImpl implements TransmittalService {
         y-=13;
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(100, y);
+        contentStream.newLineAtOffset(50, y);
         contentStream.showText(number);
         contentStream.endText();
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(225, y);
+        contentStream.newLineAtOffset(250, y);
         contentStream.showText(type);
         contentStream.endText();
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(350, y);
+        contentStream.newLineAtOffset(450, y);
         contentStream.showText(title);
         contentStream.endText();
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(475, y);
+        contentStream.newLineAtOffset(650, y);
         contentStream.showText(revision);
         contentStream.endText();
 
@@ -144,7 +147,7 @@ public class TransmittalServiceImpl implements TransmittalService {
 
     private int newLine(PDPageContentStream contentStream, int y) throws IOException {
         y-=13;
-        contentStream.newLineAtOffset(100, y);
+        contentStream.newLineAtOffset(50, y);
         return y;
     }
 
