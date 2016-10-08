9900055f7174e4fd24d6661b1d201870fd61288f
patryk.majchrzycki <patryk.majchrzycki@gmail.com>
2016-10-08 10:37:44 +0200

    CG-157 Sign out bug

--- src/main/java/pl/iticity/dbfds/controller/AuthViewController.java
+++ src/main/java/pl/iticity/dbfds/controller/AuthViewController.java
@@ -1,14 +1,26 @@
 package pl.iticity.dbfds.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.iticity.dbfds.controller.ViewController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.iticity.dbfds.service.common.PrincipalService;
 
 @Controller
 @RequestMapping(value = "/auth")
 public class AuthViewController extends ViewController {
+
+    @Autowired
+    private PrincipalService principalService;
+
     @Override
     public String getViewName() {
         return "auth";
+    }
+
+    @RequestMapping(value = "/signout", method = RequestMethod.GET)
+    public String getSignOut() {
+        principalService.unAuthenticate();
+        return "redirect:/document";
     }
 }
